664c0f27ffae13feab935ec76b27e8d039188240
hk <ryoi.matz@gmail.com>
2019-06-04 11:33:58 +0900

    update

--- handler.js
+++ handler.js
@@ -2,13 +2,15 @@
 var mathjax = require("mathjax-node");
 module.exports.main = (event, context, callback) => {
     mathjax.typeset({
-        math: event.tex,
+        math: decodeURIComponent(event.path.slice("/tex/".length)),
         format: "TeX",
         svg: true
     }, function (result) {
         var response = {
             statusCode: 200,
-            headers: {},
+            headers: { "Content-Type": "image/svg+xml" }, 
+            // メソッドレスポンスは無視され、ここで指定されたContent-Typeが使用される
+            // 指定しない場合は、メソッドレスポンス関係なく強制的にapplication/jsonが使用される
             body: result.svg
         };
         context.succeed(response);
